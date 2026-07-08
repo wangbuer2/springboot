655986edde45bbe97cad09dd36c2d277c4070841
王不二 <1741994676@qq.com>
2026-07-08 19:54:27 +0800

    comment springboot

---
+++ src/main/java/chong/wang/Main.java
@@ -0,0 +1,21 @@
+package chong.wang;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+
+@SpringBootApplication
+@EnableBatchProcessing
+public class Main {
+    // 定义日志对象
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        String str = "this is test";
+        log.info(str);
+        System.out.printf(str);
+        SpringApplication.run(Main.class, args);
+    }
+}
